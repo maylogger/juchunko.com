64e3f210c715d30741a21159a54b2e02a44ce1e8
Even <55037+maylogger@users.noreply.github.com>
2023-12-23 17:12:36 +0800

    dark mode and some config

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,18 +1,21 @@
-import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import React from "react";
+import { DocsThemeConfig } from "nextra-theme-docs";
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span className="nx-font-semibold">JUCHUNKO</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: "https://github.com/maylogger/juchunko.com",
   },
   chat: {
-    link: 'https://discord.com',
+    link: "https://discord.com",
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: "https://github.com/shuding/nextra-docs-template",
   footer: {
-    text: 'Nextra Docs Template',
+    text: "©2024 JUCHUNKO Corp. All rights reserved.",
   },
-}
+  nextThemes: {
+    defaultTheme: "dark",
+  },
+};
 
-export default config
+export default config;
