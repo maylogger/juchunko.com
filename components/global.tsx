d7a2523918f927c35c2b127ac21cec362b020915
maylogger <55037+maylogger@users.noreply.github.com>
2024-01-06 15:36:46 +0800

    add particles on home page

---
+++ components/global.tsx
@@ -0,0 +1,7 @@
+import Spline from '@splinetool/react-spline'
+
+export default function App() {
+  return (
+    <Spline scene="https://prod.spline.design/PykwbOV-m1ak0rZ0/scene.splinecode" />
+  )
+}
