bb44dfa6e0e5962020e1b7c31a466593ba28bb9f
Even <55037+maylogger@users.noreply.github.com>
2024-02-01 12:59:11 +0800

    大量更新

--- theme.config.tsx
+++ theme.config.tsx
@@ -13,8 +13,24 @@ const config: DocsThemeConfig = {
       return {
         titleTemplate: '%s – JUCHUNKO.COM',
       }
+    } else {
+      return {
+        title: '先進國會・共識未來 - JUCHUNKO.COM',
+      }
     }
   },
+  head: (
+    <>
+      <link rel="icon" type="image/svg+xml" href="/favicon.png" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta property="og:title" content="先進國會・共識未來 - JUCHUNKO.COM" />
+      <meta
+        property="og:description"
+        content="歡迎來到葛如鈞（寶博士）的寶博宇宙說明書，在這裡你可以學會如何操作、使用和召喚多元宇宙列車以及公僕寶博。"
+      />
+      <meta property="og:image" content="https://juchunko.com/ogimage.png" />
+    </>
+  ),
   footer: {
     text: '©2024 JUCHUNKO.COM All rights reserved.',
   },
